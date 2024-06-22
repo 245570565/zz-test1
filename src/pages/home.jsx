ead032880ecc093c0d0be95d8eb1c2d62309d855
zhaozhen <245570565@qq.com>
2024-06-22 16:55:50 +0800

    更新

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,20 @@
 import { Link } from 'react-router-dom';
 
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 
 function Home() {
   return (
     <header className="app-header">
-      <img src={logo} className="app-logo" alt="logo" />
+      {/* <img src={logo} className="app-logo" alt="logo" />
       <pre style={{ textAlign: 'left' }}>
         <code>window.blocklet = {JSON.stringify(window.blocklet, null, 2)}</code>
-      </pre>
+      </pre> */}
       <Link className="app-link" to="/about">
-        About
+        用户 Profile 信息
       </Link>
-      <a className="app-link" href="https://developer.blocklet.io/docs/" target="_blank" rel="noopener noreferrer">
+      {/* <a className="app-link" href="https://developer.blocklet.io/docs/" target="_blank" rel="noopener noreferrer">
         Learn Blocklet
-      </a>
+      </a> */}
     </header>
   );
 }
